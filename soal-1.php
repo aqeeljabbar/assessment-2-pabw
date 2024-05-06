c2aef81470a19e3edaac71008d856f876e45aa63
Muhammad Aqeel Jabbar <aqeeljabbr19@gmail.com>
2024-05-06 09:14:38 +0700

    V.1 Encode Data dari DB

---
+++ soal-1.php
@@ -0,0 +1,13 @@
+<?php
+
+// encode
+$dbh = new PDO('mysql:host=localhost;dbname=proyek2', 'root', '');
+$db = $dbh->prepare('SELECT * FROM stok');
+db->execute();
+$stok = $db->fectchAll(PDO::FETCH_ASSOC);
+
+// Mengubah data pada db menjadi json
+$data = json_encode($stok);
+echo $data;
+
+?>
